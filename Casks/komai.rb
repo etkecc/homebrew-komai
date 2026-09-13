d884ed95aca3518af7b3b8d87a000a6ddeba14f6
Slavi Pantaleev <slavi@devture.com>
2026-09-13 09:45:27 +0300

    fix: remove deprecated cask URL verification parameter

--- Casks/komai.rb
+++ Casks/komai.rb
@@ -2,8 +2,7 @@ cask "komai" do
   version "2026.08.24.1"
   sha256 "ef19e9ef16fd9375d7ff90dd6e51de8e4bb31129d954c7c31f1fbe5548e00efb"
 
-  url "https://github.com/etkecc/komai/releases/download/v#{version}/komai-#{version}-macos-arm64.dmg",
-      verified: "github.com/etkecc/komai/"
+  url "https://github.com/etkecc/komai/releases/download/v#{version}/komai-#{version}-macos-arm64.dmg"
   name "Komai"
   desc "Matrix chat client"
   homepage "https://github.com/etkecc/komai"
